c787308d38ea38009279db4322035e36d27671bc
Gilcemir <gangelofilho@gmail.com>
2022-05-10 10:05:12 -0300

    adding 13° problem

--- Program.cs
+++ Program.cs
@@ -14,44 +14,52 @@ static string Menu()
 
 string menu = Menu();
 
-while(menu.ToLower() != "x"){
-    switch(menu){
+while (menu.ToLower() != "x")
+{
+    switch (menu)
+    {
         case "1":
-        pe001.Get();
-        break;
+            pe001.Get();
+            break;
         case "2":
-        pe002.Get();
-        break;
+            pe002.Get();
+            break;
         case "3":
-        pe003.Get();
-        break;
+            pe003.Get();
+            break;
         case "4":
-        pe004.Get();
-        break;
+            pe004.Get();
+            break;
         case "5":
-        pe005.Get();
-        break;               
+            pe005.Get();
+            break;
         case "6":
-        pe006.Get();
-        break;
+            pe006.Get();
+            break;
         case "7":
-        pe007.Get();
-        break;          
+            pe007.Get();
+            break;
         case "8":
-        pe008.Get();
-        break;
+            pe008.Get();
+            break;
         case "9":
-        pe009.Get();
-        break;          
+            pe009.Get();
+            break;
         case "10":
-        pe010.Get();
-        break;
+            pe010.Get();
+            break;
         case "11":
-        pe011.Get();
-        break;            
+            pe011.Get();
+            break;
+        case "12":
+            pe012.Get();
+            break;
+        case "13":
+            pe013.Get();
+            break;
         default:
-        Console.WriteLine("Not implemented yet.");
-        break;
+            Console.WriteLine("Not implemented yet.");
+            break;
     }
 
     menu = Menu();
--- src/pe012.cs
+++ src/pe012.cs
@@ -1,7 +1,36 @@
-namespace ProjectEuler.src
+namespace Project
 {
     public class pe012
     {
-        
+        static List<long> triangleNumbers;
+        public static void Get()
+        {
+            triangleNumbers = new List<long>();
+            triangleNumbers.Add(0);//f(0)
+            triangleNumbers.Add(1);//f(1)
+
+            for (int i = 2; i < 12376; i++)
+            {
+                long nextTringle = triangleNumbers[i - 1] + i;
+                triangleNumbers.Add(nextTringle);
+                var divs = Divisors(nextTringle);
+                if (divs > 500)
+                    Console.WriteLine($"n = {i}, f(n) = {nextTringle}, number of divisors = {divs}");
+            }
+        }
+        public static bool IsPerferctSquare(long number) => (Math.Sqrt(number) % 1 == 0);
+
+        public static int Divisors(long n)
+        {
+            var count = 0;
+            for (int i = 1; i < Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    count += 2;
+            }
+            if (IsPerferctSquare(n))
+                return count + 1;
+            return count;
+        }
     }
 }

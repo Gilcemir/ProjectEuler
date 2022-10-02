7d660470737d8373d9b6d6479174891a2c912097
Gilcemir <gangelofilho@gmail.com>
2022-10-02 17:39:00 -0300

    add 26 problem

--- src/pe026.cs
+++ src/pe026.cs
@@ -1,10 +1,67 @@
 ﻿namespace Project;
 
-public  class pe026 : IGet
+public class pe026 : IGet
 {
+    private const int limit = 1000;
     public void Get()
     {
-        for(int i = 1; i <= 1000; i++)
-            Console.WriteLine(1/i);
+        int[] map = new int[1001];
+        var longestRecurringCycle = -1;
+        var ans = -1;
+        for(int i = 2; i <= limit; i++)
+        {
+            if(!IsDivisible(i))
+            {
+                var count = Solve(i);
+                if(count > longestRecurringCycle)
+                {
+                    longestRecurringCycle = count;
+                    ans = i;
+                }
+                
+            }
+        }
+
+        Console.WriteLine(ans);
+    }
+
+    private static bool IsDivisible(int divisor) 
+    {
+        return limit%divisor == 0;
+    }
+
+    private static int Solve(int n)
+    {
+        var denominator = SetDenominator(n);
+        var remainder = denominator%n;
+        var mapOfRemainders = new HashSet<int>
+        {
+            denominator
+        };
+        
+        while(remainder != 0 && !mapOfRemainders.Contains(remainder))
+        {
+            mapOfRemainders.Add(remainder);
+            denominator = SetDenominator(n, remainder);
+            remainder = denominator % n;
+            
+        }
+
+        
+        return mapOfRemainders.Count;
+    }
+
+    /// <summary>
+    /// Sets the next denominator. If denominator is less than the number, increases its value
+    /// to make sure that remainder will always be an integer.
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns>return the next 10^x greater than n</returns>
+    private static int SetDenominator(int n, int denominator = 10) 
+    {
+        while(denominator < n)
+            denominator *= 10;
+
+        return denominator; 
     }
 }

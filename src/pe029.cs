010cc7d3dfb3537fb835b0ec76252f85d832b877
Gilcemir <gangelofilho@gmail.com>
2022-10-16 18:36:31 -0300

    add 29 problem

---
+++ src/pe029.cs
@@ -0,0 +1,21 @@
+using System.Numerics;
+
+namespace Project;
+
+public class pe029 : IGet
+{
+    private const int _lowerLimit = 2;
+    private const int _upperLimit = 100;
+    private readonly HashSet<BigInteger> map = new HashSet<BigInteger>();
+    public void Get()
+    {
+        for(BigInteger a = _lowerLimit; a <= _upperLimit; a++)
+        {
+            for(int b = _lowerLimit; b <= _upperLimit; b++)
+            {
+                map.Add(BigInteger.Pow(a, b));
+            }
+        }
+        System.Console.WriteLine(map.Count);
+    }
+}

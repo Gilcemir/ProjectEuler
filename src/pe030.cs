b2414d0b78b5e5f398dfcba46bc667f1a05b880a
Gilcemir <gangelofilho@gmail.com>
2023-05-19 07:39:31 -0300

    add pe 30

---
+++ src/pe030.cs
@@ -0,0 +1,22 @@
+namespace Project;
+public class pe030 : IGet
+{
+
+    private readonly long[] map = new long[]
+    {
+0,
+1,
+16,
+81,
+256,
+625,
+1296,
+2401,
+4096,
+6561
+    };
+    public void Get()
+    {
+
+    }
+}
